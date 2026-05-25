71162ba7db6a2f67ee9c5b331036adb5da040703
thierry-jrobert-dot <thierry-j.robert@sfr.fr>
2026-05-25 23:42:37 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+// Fichier : data.js
+const familleData = [
+    // Branche Jean-Claude
+    { nom: "Jean-Claude ROBERT", n: "25/08/1947", f: "16/02", m: "19/10/1968", branche: "Jean-Claude" },
+    { nom: "Bernadette BULLE", n: "20/12/1946", f: "21/02", m: "19/10/1968", branche: "Jean-Claude" },
+    { nom: "Bénédicte ROBERT", n: "28/03/1970", f: "-", m: "02/07/1994", branche: "Jean-Claude" },
+    { nom: "Vincent ROBERT", n: "06/06/1971", f: "-", m: "19/08/1995", branche: "Jean-Claude" },
+    { nom: "Benoit ROBERT", n: "13/03/1982", f: "-", m: "17/07/2010", branche: "Jean-Claude" },
+    // Branche Christian
+    { nom: "Christian ROBERT", n: "15/09/1950", f: "12/11", m: "11/05/1974", branche: "Christian" },
+    { nom: "Christine TORTEY", n: "14/06/1953", f: "24/07", m: "11/05/1974", branche: "Christian" },
+    { nom: "Julien ROBERT", n: "20/07/1975", f: "-", m: "-", branche: "Christian" },
+    { nom: "Aymeric ROBERT", n: "20/07/1975", f: "-", m: "-", branche: "Christian" },
+    // Branche Elisabeth
+    { nom: "Elisabeth ROBERT", n: "12/07/1952", f: "-", m: "02/12/1972", branche: "Elisabeth" },
+    { nom: "Octave FERRUS", n: "04/07/1948", f: "-", m: "02/12/1972", branche: "Elisabeth" },
+    { nom: "David FERRUS", n: "20/06/1973", f: "-", m: "-", branche: "Elisabeth" },
+    { nom: "Hubert FERRUS", n: "25/08/1975", f: "-", m: "-", branche: "Elisabeth" },
+    { nom: "Laurent FERRUS", n: "01/05/1978", f: "-", m: "-", branche: "Elisabeth" },
+    // Branche Thierry
+    { nom: "Thierry ROBERT", n: "02/08/1957", f: "01/07", m: "01/10/1977", branche: "Thierry" },
+    { nom: "Jacqueline VANCON", n: "20/02/1956", f: "08/02", m: "01/10/1977", branche: "Thierry" },
+    { nom: "Astrid ROBERT", n: "16/06/1978", f: "27/11", m: "-", branche: "Thierry" },
+    { nom: "Aurélien ROBERT", n: "18/12/1980", f: "16/06", m: "-", branche: "Thierry" },
+    { nom: "Bérénice ANDRES", n: "21/05/2007", f: "04/02", m: "-", branche: "Thierry" }
+];
